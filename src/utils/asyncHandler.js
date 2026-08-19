b34181f7aa856eda6e969c9d61cfdc42b3c18c54
NaikNNikhil <219380549+NaikNNikhil@users.noreply.github.com>
2026-08-19 15:34:34 +0530

    utiil code write

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asynHandler= (requestHandler)=>{
+    (req,res,next)=> {
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err))
+    }
+}
+
+
+
+export {asynHandler}
+
+// const asynHandler=() => {}
+// const asynHandler=(funct)=> () => {}
+// const asynHandler=(function)=> async() => {}
+
+// const asynHandler=(fn) => async(req, res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
+
